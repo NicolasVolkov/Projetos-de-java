16238e048a65668d4cbbe77db97184637f204e77
NicolasVolkov <kroterup@gmail.com>
2026-07-30 16:49:58 -0300

    Arrumada

--- ativ.java
+++ ativ.java
@@ -3,9 +3,9 @@ public class ativ {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int[][] m = new int[3][3];
-        System.out.println("\nNúmero de linhas: " + m.length);
+        System.out.println("Número de linhas: " + m.length);
         System.out.println("Número de colunas: " + m[0].length);
-        System.out.println("\nMatriz nula:");
+        System.out.println("Matriz nula:");
         for (int i = 0; i < m.length; i++) {
             for (int j = 0; j < m[i].length; j++) { // Faz com que cada um dos elementos tenham o valor 0
                  m[i][j] = 0;
@@ -14,7 +14,7 @@ public class ativ {
             }
             System.out.println();
         }
-        System.out.println("\nMatriz identidade:");
+        System.out.println("Matriz identidade:");
         for (int i = 0; i < m.length; i++) {
             for (int j = 0; j < m[i].length; j++) {
                 if ( i == j ) { // Verifica se a posição de linha e de coluna são iguais
