5d749c2c527887d0eb5b63a47086e3c6eca83218
NicolasVolkov <kroterup@gmail.com>
2026-07-30 16:48:03 -0300

    Add files via upload

---
+++ Matriz.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class Matriz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] m = new int[3][3];
+        String simetrica = " é ";
+        String antissimetrica = " é ";
+        System.out.println("\nNúmero de linhas: " + m.length);
+        System.out.println("Número de colunas: " + m[0].length);
+        System.out.println("Digite os valores da tabela");
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                System.out.print("");
+                m[i][j] = sc.nextInt();
+            }
+            System.out.println();
+        }
+        System.out.println("\nMatriz:"); // a matriz é demonstrada
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                System.out.print(m[i][j]);
+                System.out.print("  ");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i < m.length; i++) { // verifica o tipo de matriz digitada
+            for (int j = 0; j < m[i].length; j++) {
+               if (m[i][j] != m[j][i])  {
+                   simetrica = " não é ";
+               }
+                if (m[i][j] != -(m[j][i]))  {
+                    antissimetrica = " não é ";
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("A matriz"+ simetrica+ "simétrica.");
+        System.out.println("A matriz"+ antissimetrica+ "antissimétrica.");
+        sc.close();
+    }
+}
+
+
+
+
+
+
+
---
+++ ativ.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class ativ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] m = new int[3][3];
+        System.out.println("\nNúmero de linhas: " + m.length);
+        System.out.println("Número de colunas: " + m[0].length);
+        System.out.println("\nMatriz nula:");
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) { // Faz com que cada um dos elementos tenham o valor 0
+                 m[i][j] = 0;
+                System.out.print(m[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        System.out.println("\nMatriz identidade:");
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[i].length; j++) {
+                if ( i == j ) { // Verifica se a posição de linha e de coluna são iguais
+                    m[i][j] = 1;
+                } else {
+                    m[i][j] = 0;
+                }
+                System.out.print(m[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
